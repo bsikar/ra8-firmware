b0aea1968d556d18abeb7364f5785c0ca85931a0
Brighton Sikarskie <bsikar@tuta.io>
2026-05-08 16:25:38 -0500

    ci: untrack build_test/ and doxy_run.txt accidentally committed in 6d3ce4830
    These are local-only build artifacts; they should never have been
    staged. Add to .gitignore and remove from index.

--- build_test/bench/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/bsikar/Documents/github/ra8d2-firmware/tests/bench
-# Build directory: /Users/bsikar/Documents/github/ra8d2-firmware/build_test/bench
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
